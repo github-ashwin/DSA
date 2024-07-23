a600f097dc9b03b15d8a191f94d27f62407adcba
github-ashwin <ashi.github@gmail.com>
2024-07-23 19:58:30 +0530

    Finding paths in Graph

--- Graph/graph.py
+++ Graph/graph.py
@@ -12,8 +12,23 @@ class Graph:
         
         print(self.graph_dict)
 
-    def get_paths(self,start,end):
-        pass
+    def get_paths(self,start,end,path=[]):
+        path = path + [start]
+
+        if start == end:
+            return [path]
+        
+        if start not in self.graph_dict:
+            return []
+        
+        paths = []
+        for node in self.graph_dict[start]:
+            if node not in path:
+                new_path = self.get_paths(node,end,path)
+                for p in new_path:
+                    paths.append(p)
+        
+        return paths
 
 if __name__ == '__main__':
     routes = [
@@ -26,3 +41,8 @@ if __name__ == '__main__':
     ]
 
     route_graph = Graph(routes)
+
+    start = "Mumbai"
+    end = "New York"
+
+    print(f"All paths between: {start} and {end}: ",route_graph.get_paths(start,end))
