d666204329c267cd92067b28c014d5ab27a1519d
github-ashwin <ashi.github@gmail.com>
2024-07-24 20:43:30 +0530

    Adding shortest path

--- Graph/graph.py
+++ Graph/graph.py
@@ -30,14 +30,46 @@ class Graph:
         
         return paths
 
+    def get_shortest_path(self, start, end, path=[]):
+        path = path + [start]
+
+        if start == end:
+            return path
+
+        if start not in self.graph_dict:
+            return None
+
+        shortest_path = None
+        for node in self.graph_dict[start]:
+            if node not in path:
+                sp = self.get_shortest_path(node, end, path)
+                if sp:
+                    if shortest_path is None or len(sp) < len(shortest_path):
+                        shortest_path = sp
+
+        return shortest_path
+
 if __name__ == '__main__':
+
     routes = [
-        ("Mumbai","Paris"),
-        ("Mumbai","Dubai"),
-        ("Paris","Dubai"),
-        ("Paris","New York"),
-        ("Dubai","New York"),
-        ("New York","Toronto")
+        ("Mumbai","Pune"),
+        ("Mumbai", "Surat"),
+        ("Surat", "Bangaluru"),
+        ("Pune","Hyderabad"),
+        ("Pune","Mysuru"),
+        ("Hyderabad","Bangaluru"),
+        ("Hyderabad", "Chennai"),
+        ("Mysuru", "Bangaluru"),
+        ("Chennai", "Bangaluru")
+    ]
+
+    routes = [
+        ("Mumbai", "Paris"),
+        ("Mumbai", "Dubai"),
+        ("Paris", "Dubai"),
+        ("Paris", "New York"),
+        ("Dubai", "New York"),
+        ("New York", "Toronto"),
     ]
 
     route_graph = Graph(routes)
@@ -46,3 +78,10 @@ if __name__ == '__main__':
     end = "New York"
 
     print(f"All paths between: {start} and {end}: ",route_graph.get_paths(start,end))
+    print(f"Shortest path between {start} and {end}: ", route_graph.get_shortest_path(start,end))
+
+    start = "Dubai"
+    end = "New York"
+
+    print(f"All paths between: {start} and {end}: ",route_graph.get_paths(start,end))
+    print(f"Shortest path between {start} and {end}: ", route_graph.get_shortest_path(start,end))
