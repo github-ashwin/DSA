934b7c63e9aebd17edd8bd642201663db1075d4c
github-ashwin <ashi.github@gmail.com>
2024-08-18 19:08:42 +0530

    Build Array from Permutation - Leetcode

---
+++ Leetcode - Python/BuildArrayPermutations.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def buildArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        result = []
+        for i in nums:
+            result.append(nums[i])
+        
+        return result
