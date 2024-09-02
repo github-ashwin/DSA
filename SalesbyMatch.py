8872afad8272dfd7c828895b8d865b20bfb5934f
github-ashwin <ashi.github@gmail.com>
2024-09-02 20:07:05 +0530

    Sales by Match - HackerRank

---
+++ SalesbyMatch.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    socks = {}
+    for item in ar:
+        if item in socks:
+            socks[item] += 1
+        else:
+            socks[item] = 1
+    
+    result = 0
+    for i in socks.values():
+        result += i//2
+    
+    return result
+        
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
