0f5c342861bedcc3c89368ee2d0392ccd47d049f
github-ashwin <ashi.github@gmail.com>
2024-09-21 19:28:53 +0530

    Find Digits - HackerRank

---
+++ FindDigits.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'findDigits' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def findDigits(n):
+    # Write your code here
+    result = 0
+    num = [int(digit) for digit in str(n)]
+    for i in num:
+        if i!=0 and n%i == 0:
+            result +=1
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = findDigits(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
