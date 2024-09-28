7ef9bed45c884681f8af909544ecf2e6142e793f
github-ashwin <ashi.github@gmail.com>
2024-09-28 22:21:34 +0530

    Jumping on the Clouds - HackerRank

---
+++ JumpingOnClouds.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'jumpingOnClouds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY c as parameter.
+#
+
+def jumpingOnClouds(c):
+    # Write your code here
+    pos = 0
+    jump = 0
+    while pos < len(c)-1:
+        if pos+2 < len(c) and c[pos+2]==0:
+            pos +=2
+        else:
+            pos +=1
+        jump +=1
+    
+    return jump
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
