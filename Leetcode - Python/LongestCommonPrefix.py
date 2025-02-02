bde799272201dcfd380351aad1003e1c4fcd10b8
github-ashwin <ashi.github@gmail.com>
2025-02-02 20:55:10 +0530

    Longest Common Prefix

---
+++ Leetcode - Python/LongestCommonPrefix.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        prefix = strs[0]
+
+        for s in strs[1:]:
+            while not s.startswith(prefix):
+                prefix = prefix[:-1]
+                if not prefix:
+                    return ""
+        return prefix
+        
